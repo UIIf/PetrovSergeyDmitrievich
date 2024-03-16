9c215e22b02c3c8cc4e54388adfe957362957a9a
Сергей <uiif.github@gmail.com>
2024-03-16 17:13:17 +1000

    Smth like socnet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -55,16 +62,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> citates = [
+      'Если волк молчит, то лучше его не перебивать',
+     'Если тебя обсуждают за спиной, значит ты впереди',
+     'Волк слабее льва и тигра но в цырке не выступает',
+     'Ты живешь как карта ляжет, я жвиу как мама скажет',
+     'Работа не волк, работа ворк',
+    ];
+  String citate = "Обычная волчья цитата";
 
-  void _incrementCounter() {
+  void _changeCitate() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      citate = citates[Random().nextInt(citates.length)];
     });
   }
 
@@ -84,42 +98,91 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Мнгновенофото"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          children: [
+              Container(
+                width: 800,
+                color: Color.fromARGB(255, 67, 90, 238),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Петров Сергей Дмитриевич",
+                      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start, children: [
+                      const Icon(
+                        Icons.person,
+                        size: 100,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                        Text("$citate"),
+                        TextButton(
+                          onPressed: _changeCitate,
+                          child: Icon(
+                            Icons.play_arrow, 
+                            size:  20,
+                            color: Color.fromARGB(255, 233, 134, 28)
+                          )
+                        ),
+                      ],)
+                    ]),
+                ],)
+              ,),
+              Container(
+                width: 800,
+                color: Color.fromARGB(255, 145, 192, 239),
+                child: 
+                  Column(
+                    children: [
+                      Container(
+                        
+                        margin: EdgeInsets.all(10),
+                        height: 500,
+                        width: 400,
+                        color: Color.fromARGB(255, 175, 190, 255),
+                        child: 
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: 50,
+                              width:400,
+                              color:Color.fromARGB(255, 85, 118, 235),
+                              child:const Text("Живопись русских комуналок", style: TextStyle(fontWeight: FontWeight.bold),),  
+                            ),
+                            Icon(Icons.landscape, size:300),
+                            Container(
+                              width: 350,
+                              height: 50,
+                              color: Color.fromARGB(249, 156, 139, 255),
+                              child:
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Icon(Icons.share),
+                                    Icon(Icons.monitor_heart)
+                                  ],
+                                )
+                            )
+                          ],)
+                      )
+                      
+                    ],
+                  )
+              ),
+            // Post holde
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
